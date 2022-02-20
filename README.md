cea0ae4bc647fda65b4db6412c685a2a80eb4d67
Brice Folleas <brice.folleas@gmail.com>
2022-02-20 23:20:56 +0100

    conf: add gulp conf

--- README.md
+++ README.md
@@ -48,9 +48,49 @@ npm install --save-dev gulp
 
 Create new file `gulpfile.js` and then create a task using `gulp` module
 
+#### Dist folder
+
 Then, following command to run the task (here task name is *`processHTML`*)
 ```sh
 gulp processHTML
 ```
 
 Which creates a new `dist` folder containing the `index.html` file
+
+#### Linting 
+
+```sh
+npm install --save-dev jshint gulp-jshint
+```
+
+#### Transpilling 
+
+```sh
+npm install --save-dev gulp-babel
+```
+
+If the following error appears when launching the tasks...
+`Error: Cannot find module gulp-babel`
+
+... use this command to install right version of `gulp-babel`
+```sh
+npm install gulp-babel@8.0.0
+```
+
+#### Minifying
+
+```sh
+npm install --save-dev gulp-uglify
+```
+
+#### Automating commands
+
+```sh
+npm install --save-dev run-sequence
+```
+
+#### Live reload
+
+```sh
+npm install --save-dev browser-sync
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 const gulp = require('gulp'); // require Gulp module methods within our file
+const jshint = require('gulp-jshint');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const runSequence = require('run-sequence');
+const browserSync = require('browser-sync').create();
 
 // Simple task which takes all HTML files in our project directory and copies them to a 'dist' folder
 // src function to select input file
@@ -6,6 +11,57 @@ const gulp = require('gulp'); // require Gulp module methods within our file
 // Here, as the operation is asynchronous, you must use the async keyword in the declaration of the function
 // Otherwise you get an 'did you forget to signal async' error
 gulp.task('processHTML', async () => {
-    gulp.src('index.html')
+    gulp.src('*.html')
         .pipe(gulp.dest('dist'));
 });
+
+// Task for javascript files
+// 1 : initialize JSHint and tell it to lint for ES6;
+// 2 : tell JSHint which reporter to use to show any linting errors;
+// 3 : configure babel (instead of .babelrc)
+// 4 : minfying via uglify
+// 5 : copy it to dist folder.
+gulp.task('processJS', () => {
+    gulp.src('scripts.js')
+        .pipe(jshint({
+            esversion: 6
+        }))
+        .pipe(jshint.reporter('default'))
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Task to resolve the babel-polyfill issue
+gulp.task('babelPolyfill', () => {
+    gulp.src('node_modules/babel-polyfill/browser.js')
+        .pipe(gulp.dest('dist/node_modules/babel-polyfill'));
+});
+
+// Task to set up our development server based on our production files
+gulp.task('browserSync', () => {
+    browserSync.init({
+        server: './dist',
+        port: 8080,
+        ui: {
+            port: 8081
+        }
+    });
+});
+
+// Watch changes brought to these files
+gulp.task('watch', ['browserSync'], () => {
+    gulp.watch('*.js', ['processJS']);
+    gulp.watch('*.html', ['processHTML']);
+
+    gulp.watch('dist/*.js', browserSync.reload);
+    gulp.watch('dist/*.html', browserSync.reload);
+});
+
+// A task to run them all
+// gulp.task('default', (callback) => {
+//     runSequence(['processHTML', 'processJS', 'babelPolyfill'], 'watch', callback);
+// });
+
